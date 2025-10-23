ec47f81bc0fda5d713e20bc419a6562a8cfb8956
Moamen-Wady <moamen.wady@gmail.com>
2025-10-23 12:53:39 +0300

    final

--- src/components/vidBanner.jsx
+++ src/components/vidBanner.jsx
@@ -1,32 +1,46 @@
-import { useRef } from "react";
+//ORIGINAL LECTRO COMMENTED
+// import { useRef } from "react";
+// import "./vbanner.css";
+// import useIntersectionObserver from "@react-hook/intersection-observer";
+
+// export default function VidBanner() {
+//   const containerRef = useRef();
+//   const lockRef = useRef(false);
+//   const { isIntersecting } = useIntersectionObserver(containerRef);
+//   if (isIntersecting) {
+//     lockRef.current = true;
+//   }
+
+//   return (
+//     <div className="vbanw" >
+//       <header>
+//         <h1 className="vh1">About Lectrobar</h1>
+//         <div className="vvid" ref={containerRef}>
+//           <img loading="lazy" src="/btw.webp" alt="" />
+//           {lockRef.current && (
+//             <video autoPlay muted loop>
+//             the video is available on google drive
+//               <source src="/lectro.webm" type="video/webm"></source>
+//             </video>
+//           )}
+//             </div>
+//       </header>
+//     </div>
+//   );
+// };
+
 import "./vbanner.css";
-import useIntersectionObserver from "@react-hook/intersection-observer";
 
-export default function VidBanner() {
-  const containerRef = useRef();
-  const lockRef = useRef(false);
-  const { isIntersecting } = useIntersectionObserver(containerRef);
-  if (isIntersecting) {
-    lockRef.current = true;
-  }
-
+export default function Blogb() {
   return (
-    <div className="vbanw" >
+    <div className="vbanw">
+      <img src="/btw.webp" alt="" className="vshade" />
       <header>
         <h1 className="vh1">About Lectrobar</h1>
-        <div className="vvid" ref={containerRef}>
-          <img loading="lazy" src="/btw.webp" alt="" />
-          {lockRef.current && (
-            // <video autoPlay muted loop>
-            //the video is available on google drive
-            //   <source src="/lectro.webm" type="video/mp4"></source>
-            // </video>
-                        <video autoPlay muted loop>
-              <source src="https://github.com/Moamen-Wady/Lectrobar/blob/main/public/lectro.webm" type="video/mp4"></source>
-            </video>
-          )}
-            </div>
+        <div className="vvid">
+          <img src="/blogb.webp" alt="" />
+        </div>
       </header>
     </div>
   );
-};
+}
